bef38337cc9a0929cb393d1b10ec3670187b9d6a
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2023-06-22 18:23:44 +0700

    tampilan register form

--- lib/view/registerpage.dart
+++ lib/view/registerpage.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:redlenshoescleaning/view/registerpage.dart';
+import 'package:redlenshoescleaning/view/loginpage.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
 
   // String? username;
@@ -45,10 +45,10 @@ class _LoginPageState extends State<LoginPage> {
                       const SizedBox(height: 20),
                       Container(
                         margin: const EdgeInsets.only(
-                          right: 220,
+                          right: 250,
                         ),
                         child: const Text(
-                          'Username',
+                          'Nama',
                           style: TextStyle(
                             color: Colors.black,
                             fontWeight: FontWeight.bold,
@@ -60,7 +60,30 @@ class _LoginPageState extends State<LoginPage> {
                         padding: const EdgeInsets.symmetric(horizontal: 50),
                         child: TextFormField(
                           decoration: const InputDecoration(
-                            hintText: 'Username',
+                            hintText: 'Nama',
+                            hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        margin: const EdgeInsets.only(
+                          right: 250,
+                        ),
+                        child: const Text(
+                          'Email',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Email',
                             hintStyle: TextStyle(fontStyle: FontStyle.italic),
                           ),
                         ),
@@ -90,6 +113,31 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                       ),
+                      const SizedBox(height: 10),
+                      Container(
+                        margin: const EdgeInsets.only(
+                          right: 155,
+                        ),
+                        child: const Text(
+                          'Confirm Password',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: 'Confirm Password',
+                            hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                            suffixIcon: Icon(Icons.remove_red_eye),
+                          ),
+                        ),
+                      ),
                       const SizedBox(height: 40),
                       ElevatedButton(
                         onPressed: () {
@@ -108,7 +156,7 @@ class _LoginPageState extends State<LoginPage> {
                           minimumSize: const Size(280, 50),
                         ),
                         child: const Text(
-                          'Login',
+                          'Register',
                           style: TextStyle(
                             fontSize: 18,
                             color: Colors.white,
@@ -118,18 +166,18 @@ class _LoginPageState extends State<LoginPage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Text("Don't have an account?"),
+                          const Text("Already have an account?"),
                           TextButton(
                             onPressed: () {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => const RegisterPage(),
+                                  builder: (context) => const LoginPage(),
                                 ),
                               );
                             },
                             child: const Text(
-                              'Sign Up',
+                              'Sign In',
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 color: Colors.red,
