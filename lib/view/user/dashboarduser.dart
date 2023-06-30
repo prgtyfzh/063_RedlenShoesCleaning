b5fb8340f3a517bf32f72b89e1f921509645c1fd
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2023-07-01 05:30:33 +0700

    memperbaiki dashboard user

--- lib/view/user/dashboarduser.dart
+++ lib/view/user/dashboarduser.dart
@@ -34,54 +34,34 @@ class _DashboardUserState extends State<DashboardUser> {
                     vertical: 5.0,
                     horizontal: 20.0,
                   ),
-                  child: Card(
-                    color: const Color(0xFFD9D9D9),
-                    elevation: 4,
-                    child: ListTile(
-                      title: const Text('Tanggal'),
-                      subtitle: const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Sepatu'),
-                          Text('Nama Treatment'),
-                          Text('Status'),
-                        ],
-                      ),
-                      trailing: PopupMenuButton<String>(
-                        icon: const Icon(
-                          Icons.more_vert,
-                          size: 25,
+                  child: InkWell(
+                    onLongPress: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) => UpdateContact(
+                      //       id: data[index]['id'],
+                      //       name: data[index]['name'],
+                      //       phone: data[index]['phone'],
+                      //       email: data[index]['email'],
+                      //       address: data[index]['address'],
+                      //     ),
+                      //   ),
+                      // );
+                    },
+                    child: const Card(
+                      color: Color(0xFFD9D9D9),
+                      elevation: 4,
+                      child: ListTile(
+                        title: Text('Tanggal'),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Sepatu'),
+                            Text('Nama Treatment'),
+                            Text('Status'),
+                          ],
                         ),
-                        onSelected: (value) {
-                          if (value == 'edit') {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //     builder: (context) =>
-                            //         // const UpdatePengeluaran(
-                            //         //     // id: data[index]['id'],
-                            //         //     // name: data[index]['name'],
-                            //         //     // phone: data[index]['phone'],
-                            //         //     // email: data[index]['email'],
-                            //         //     // address: data[index]['address'],
-                            //         //     ),
-                            //   ),
-                            // );
-                          } else if (value == 'delete') {
-                            // Handle delete action
-                            // ...
-                          }
-                        },
-                        itemBuilder: (context) => [
-                          const PopupMenuItem(
-                            value: 'edit',
-                            child: Text('Edit'),
-                          ),
-                          const PopupMenuItem(
-                            value: 'delete',
-                            child: Text('Delete'),
-                          ),
-                        ],
                       ),
                     ),
                   ),
